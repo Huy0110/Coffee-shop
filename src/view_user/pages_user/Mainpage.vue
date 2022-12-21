3e02382f7ea34acbfcccebe939fe8664d361c24b
phtchung <phamthanhchung2001@gmail.com>
2022-12-21 13:25:20 +0700

    Create Mainpage

--- src/view_user/pages_user/Mainpage.vue
+++ src/view_user/pages_user/Mainpage.vue
@@ -1,10 +1,34 @@
 <template>
-$END$
+<v-container>
+   <v-row>
+<!--     ảnh to mainpage , chiếm 1/2 width -->
+     <v-col cols="12" md="6" sm="12" lg="6">
+        <div>
+          <a href="#">
+            <img src="https://file.hstatic.net/1000075078/file/banner_app_59792ee4e6074b33aca7f140433e9292.jpg" alt="">
+          </a>
+        </div>
+     </v-col>
+      <v-row>
+        <v-col cols="12" md="6" lg="6" sm="6">
+          <Item></Item>
+        </v-col>
+        <v-col cols="12" md="6" lg="6" sm="6"></v-col>
+      </v-row>
+
+   </v-row>
+   <v-row>
+
+   </v-row>
+</v-container>
 </template>
 
 <script>
 export default {
-name: "Mainpage.vue"
+  name: "Mainpage.vue",
+  components:{
+    Item: () => import('@/components/Item'),
+  }
 }
 </script>
 
