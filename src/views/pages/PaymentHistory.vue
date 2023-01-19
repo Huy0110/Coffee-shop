10d0101ac38d59e2f251cc34722d833b99e244bb
phtchung <phamthanhchung2001@gmail.com>
2023-01-19 21:44:39 +0700

    add method check total

--- src/views/pages/PaymentHistory.vue
+++ src/views/pages/PaymentHistory.vue
@@ -29,11 +29,11 @@
                 <v-row>
                   <v-col cols="12" sm="6">
                     <v-row class="mt-4 grey--text text--darken-2 font-weight-regular" justify="center" align="center" >TỔNG TIỀN GD TRONG NGÀY</v-row>
-                    <v-row class="mt-3 title black--text text--darken-2 font-weight-regular text-h5 " justify="center" align="center" > {{SaleTotal}} </v-row>
+                    <v-row class="mt-3 title black--text text--darken-2 font-weight-regular text-h5 " justify="center" align="center" > {{saleTotal}}VNĐ </v-row>
                   </v-col>
                   <v-col cols="12" sm="6">
                     <v-row class="mt-4 grey--text text--darken-2 font-weight-regular" justify="center" align="center" >TỔNG SỐ GD TRONG NGÀY</v-row>
-                    <v-row class="mt-3 title black--text text--darken-2 font-weight-regular text-h5 " justify="center" align="center" >6</v-row>
+                    <v-row class="mt-3 title black--text text--darken-2 font-weight-regular text-h5 " justify="center" align="center" >{{countOrders}}</v-row>
                   </v-col>
                 </v-row>
               </v-col>
@@ -74,14 +74,9 @@ export default {
   data () {
     return {
       search: '',
+      count:0,
       headers: [
-        {
-          text: 'THỜI ĐIỂM',
-          align: 'start',
-          sortable: false,
-          value: 'time',
-          width:'16%',
-        },
+        {text: 'THỜI ĐIỂM', align: 'start', sortable: false, value: 'time', width:'16%',},
         { text: 'Mã GIAO DỊCH', value: 'tradingcode' ,width: '16%'},
         { text: 'SỐ TIỀN', value: 'cash',width: '14%' },
         { text: 'TÌNH TRẠNG ', value: 'status',width: '14%' },
@@ -91,42 +86,35 @@ export default {
         {
           time: '10/12/2022 19:02',
           tradingcode: '22121000003892',
-          cash: '40.000VNĐ',
+          cash: '40000',
           status: 'Thành công',
           note: 'The Coffee House Bách Khoa',
         },
         {
           time: '10/12/2022 19:05',
           tradingcode: '22121000004493',
-          cash: '140.000VNĐ',
+          cash: '140000',
           status: 'Thành công',
           note: 'The Coffee House Bách Khoa',
         },
         {
           time: '10/12/2022 19:10',
           tradingcode: '22121000008324',
-          cash: '100.000VNĐ',
+          cash: '100000',
           status: 'Thành công',
           note: 'The Coffee House Bách Khoa',
         },
         {
           time: '10/12/2022 19:15',
           tradingcode: '22121000009462',
-          cash: '40.000VNĐ',
+          cash: '40000',
           status: 'Thành công',
           note: 'The Coffee House Bách Khoa',
         },
         {
           time: '10/12/2022 19:39',
           tradingcode: '22121000009811',
-          cash: '60.000VNĐ',
-          status: 'Thành công',
-          note: 'The Coffee House Bách Khoa',
-        },
-        {
-          time: '10/12/2022 19:28',
-          tradingcode: '22121000003821',
-          cash: '50.000VNĐ',
+          cash: '60000',
           status: 'Thành công',
           note: 'The Coffee House Bách Khoa',
         },
@@ -134,19 +122,24 @@ export default {
     }
   },
   computed:{
-    SaleTotal(){
-      let total =0;
-      let a = this.contents.map(content => Number(content.cash.slice(0,content.cash.length-3)));
-      for(let i=0;i<a.length;i++){
-          total += (a[i]);
-      }
-      if(Number.isInteger(total)){
-        return total+'.000VNĐ';
-      }else
-        return total+'VNĐ';
 
-       // return a.reduce((accumulator, currentValue) => accumulator + currentValue,0);
+    saleTotal(){
+      let total = 0;
+      for(let i = 0 ; i < this.contents.length ; i++){
+          total += parseInt(this.contents[i].cash)
+      }
+      return this.separator(total);
+    },
+    countOrders(){
+      return this.contents.length;
     }
   },
+  methods:{
+    separator(numb) {
+      var str = numb.toString().split(".");
+      str[0] = str[0].replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+      return str.join(".");
+    },
+  }
 };
 </script>
