9127ad43fe9536055e65bf3c16679b09dacca43c
Huy0110 <duchuy01102001@gmail.com>
2023-02-07 21:35:08 +0700

    add items change when header change

--- src/views/pages/Icons.vue
+++ src/views/pages/Icons.vue
@@ -2,16 +2,27 @@
 <v-container>
   <View_Header></View_Header>
   <v-tabs fixed-tabs>
-    <v-tab v-for="menuItem in menuItems" :key="menuItem.name" @click="menuType=false">
+    <v-tab v-for="(menuItem, index) in menuItems" :key="index" @click="menuType=index">
       {{menuItem.name}}
     </v-tab>
   </v-tabs>
 
 <!-- need to process menuType when click each category -->
+<div>
+  <div v-if="menuType == 0">
+    <menuMenu />
+  </div>
+  <div v-else-if="menuType == 1">
+    <menuOthers :menuType=1 />
+  </div>
+  <div v-else-if="menuType == 2">
+    <menuOthers :menuType=2 />
+  </div>
+</div>
   <!-- <menuCategory :menuType="category"/> -->
 
 
-  <menuMenu/>
+  <!-- <menuMenu/> -->
   <Footer></Footer>
 
 </v-container>
@@ -23,17 +34,18 @@ export default {
 
   data() {
     return {
-      menuType: false,
+      menuType: 0,
       menuItems: [
-          {name: 'Tra'},
-          {name: 'Coffee'},
-          {name: 'Menu'},
+        {name: 'Menu'},
+        {name: 'Coffee'},
+        {name: 'Tra'},
       ],
   }},
   components: {
       // Item: () => import('@/components/Item'),
       // menuCategory: () => import('@/components/adminComponents/menuCategory'),
       menuMenu: () => import('@/components/adminComponents/menuMenu'),
+      menuOthers: () => import('@/components/adminComponents/menuOthers'),
       View_Header: () => import('@/layouts/header/View_Header'),
       Footer: () => import('@/layouts/footer/Footer'),
   }
