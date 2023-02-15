b2102f4b9c72a41bba17c27dc467ab0ccad56b79
Huy0110 <duchuy01102001@gmail.com>
2023-02-15 19:13:47 +0700

    add logged in condition

--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -1,48 +1,86 @@
 <template>
-  <v-app id="inspire">
+<v-app id="inspire">
 
-    <Sidebar :expand-on-hover.sync="expandOnHover"></Sidebar>
+    <Sidebar :expand-on-hover.sync="expandOnHover" :class="{notActiveLogged: !loggin}"></Sidebar>
     <v-content>
-      <v-container rounded-0 class="fill-height" fluid>
-        <router-view />
-      </v-container>
+        <v-container rounded-0 class="fill-height" fluid>
+            <router-view />
+        </v-container>
     </v-content>
-  </v-app>
+</v-app>
 </template>
 
 <script>
 // import Header from "./header/Header";
 import Sidebar from "./sidebar/Sidebar";
-import { mapState, mapMutations } from "vuex";
+import {
+    mapState,
+    mapMutations
+} from "vuex";
 export default {
-  name: "Layout",
+    name: "Layout",
 
-  components: {
-    // Header,
-    Sidebar
-  },
+    components: {
+        // Header,
+        Sidebar
+    },
 
-  props: {
-    source: String
-  },
-  data: () => ({
-    expandOnHover: false
-  }),
-  computed: {
-    ...mapState(["Customizer_drawer"])
-  },
+    props: {
+        source: String
+    },
+    data: () => ({
+        expandOnHover: false,
+        loggin: false,
+    }),
+    computed: {
+        ...mapState(["Customizer_drawer"]),
 
-  methods: {
-    ...mapMutations({
-      setCustomizerDrawer: "SET_CUSTOMIZER_DRAWER"
-    })
-  }
+    },
+
+    mounted() {
+        window.addEventListener('admin-logged-in', (event) => {
+            console.log("here: ", event.detail.logged_in)
+            if (event.detail.logged_in == false) {
+                this.loggin = false
+            } else {
+                this.loggin = true
+            }
+            // console.log("item count in mounted: ", this.itemCount)
+        });
+
+        console.log(localStorage.getItem("AdminLoggedIn"))
+        if (localStorage.getItem("AdminLoggedIn") == "true") {
+            console.log("Thanh cong")
+            this.loggin = true
+        } else {
+          console.log("That bai")
+            this.loggin = false
+        }
+    },
+    created() {
+        if (localStorage.getItem("AdminLoggedIn") == true) {
+            console.log("Thanh cong")
+            this.loggin = true
+        } else {
+            this.loggin = false
+        }
+    },
+
+    methods: {
+        ...mapMutations({
+            setCustomizerDrawer: "SET_CUSTOMIZER_DRAWER"
+        })
+    }
 };
 </script>
 
 <style>
+
+.notActiveLogged {
+  display: none;
+}
 .fill-height {
-  align-items: start !important;
-  margin-top: 42px;
+    align-items: start !important;
+    margin-top: 42px;
 }
 </style>
